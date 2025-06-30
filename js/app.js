677dd6e22e3272740054f406dfaf82a2202f1544
coderemon24 <mdemonahmed2021@gmail.com>
2025-06-30 16:13:31 +0600

    slick slider added to news section

--- js/app.js
+++ js/app.js
@@ -66,40 +66,68 @@ backToTopBtn.addEventListener("click", () => {
   });
 });
 
-
 $(document).ready(function () {
+  // Mobile Menu
   let menuBtn = $("#menu-btn");
   let navMenu = $("#nav-menu");
   let overlay = $("#overlay");
   let humburger = $("#humburger");
   let close = $("#close");
-  
-  menuBtn.on('click', function () {
-    
-    humburger.toggleClass('hidden');
-    close.toggleClass('hidden');
-    
-    if (navMenu.hasClass('w-[0%]')) {
-      navMenu.removeClass('w-[0%]').addClass('w-[60%]');
-      overlay.removeClass('hidden').addClass('flex');
+
+  menuBtn.on("click", function () {
+    humburger.toggleClass("hidden");
+    close.toggleClass("hidden");
+
+    if (navMenu.hasClass("w-[0%]")) {
+      navMenu.removeClass("w-[0%]").addClass("w-[60%]");
+      overlay.removeClass("hidden").addClass("flex");
     } else {
-      navMenu.removeClass('w-[60%]').addClass('w-[0%]');
-      overlay.removeClass('flex').addClass('hidden');
+      navMenu.removeClass("w-[60%]").addClass("w-[0%]");
+      overlay.removeClass("flex").addClass("hidden");
     }
-    
-    if(overlay.hasClass('flex'))
-    {
-      overlay.on('click', function () {
-        if(!close.hasClass('hidden'))
-        {
-          close.toggleClass('hidden');
-          humburger.toggleClass('hidden');
+
+    if (overlay.hasClass("flex")) {
+      overlay.on("click", function () {
+        if (!close.hasClass("hidden")) {
+          close.toggleClass("hidden");
+          humburger.toggleClass("hidden");
         }
-        
-        navMenu.removeClass('w-[60%]').addClass('w-[0%]');
-        overlay.removeClass('flex').addClass('hidden');
+
+        navMenu.removeClass("w-[60%]").addClass("w-[0%]");
+        overlay.removeClass("flex").addClass("hidden");
       });
     }
-    
   });
+
+  // slick slider
+  $('.news-slider').slick({
+    dots: true,
+    arrows: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+  
+  
 });
